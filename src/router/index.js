17947d1a45dc28434c70b6e897ba1a3a4633aa32
wangrui <wrui1116@163.com>
2018-11-09 10:16:24 +0800

    王瑞

--- src/router/index.js
+++ src/router/index.js
@@ -1,37 +1,46 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import ceshi from '@/vues/ceshi.vue'
-import ceshia from '@/vues/ceshia.vue'
-import ceshib from '@/vues/ceshib.vue'
-// import zhou from '@/vues/zhou'
-Vue.use(Router)
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import ceshi from "@/vues/ceshi.vue";
+import ceshia from "@/vues/ceshia.vue";
+import ceshib from "@/vues/ceshib.vue";
+
+import zhou from "@/vues/zhou.vue";
+import zheng from "@/vues/zheng.vue";
+
+Vue.use(Router);
 
 export default new Router({
-  routes: [{
-      path: '/',
-      name: 'HelloWorld',
+  routes: [
+    {
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
     },
     {
-      path: '/ceshi',
-      name: 'ceshi',
+      path: "/ceshi",
+      name: "ceshi",
       component: ceshi
     },
     {
-      path: '/ceshia',
-      name: 'ceshia',
+      path: "/ceshia",
+      name: "ceshia",
       component: ceshia
     },
     {
-      path: '/ceshib',
-      name: 'ceshib',
+      path: "/ceshib",
+      name: "ceshib",
       component: ceshib
     },
-  //   {
-  //   path: '/zhou',
-  //   name: 'zhou',
-  //   component: zhou
-  // }
-]
-})
+    {
+      path: "/zhou",
+      name: "zhou",
+      component: zhou
+    },
+    {
+      path: "/zheng",
+      name: "zheng",
+      component: zheng
+    }
+  ]
+});
